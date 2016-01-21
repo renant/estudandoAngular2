d8b4d7a7a14b24df0669116da61fa680011be225
Renan Teixeira <renan_replay@live.com>
2016-01-21 10:58:57 -0200

    Listagem do cadastro usando ngFor e ngIf

--- app/ts/car.component.ts
+++ app/ts/car.component.ts
@@ -13,9 +13,21 @@ interface Car{
 
 export class CarComponent{
     public title:string = "Cadastro de Veiculos";
-    public car: Car = {
-        id:1,
-        brand:'BMW',
-        model:'X6',
+    public cars:Car[] = Cars;
+    public selectedCar: Car;
+    
+    public onSelect(car: Car): void{
+        this.selectedCar = car;
     }
+    
 }
+
+var Cars: Car[] = [
+    {id: 1, brand: "BMW", model: "X6"},
+    {id: 2, brand: "BMW", model: "X1"},
+    {id: 3, brand: "Volkswagen", model: "Golf GTI"},
+    {id: 4, brand: "Chevrolet", model: "Cruze"},
+    {id: 5, brand: "Ford", model: "Fusion"},
+    {id: 6, brand: "Fiat", model: "Bravo"},
+    {id: 7, brand: "Mercedes", model: "C180"}
+]
